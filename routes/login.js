1f75bd51e71f89eda65664953a28baa59606b05c
TonyZXY <xyzheng109@gmail.com>
2018-06-18 15:18:06 +1000

    update address

--- routes/login.js
+++ routes/login.js
@@ -8,7 +8,7 @@ const options = {
     pass: "appdevgkV6="
 };
 
-mongoose.connect('mongodb://10.10.6.111:27017/BGLNewsAppbkend', options);
+mongoose.connect('mongodb://localhost/BGLNewsAppbkend', options);
 
 module.exports = router;
 
