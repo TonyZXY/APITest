b265237c2dab9802b5e18d2247faa6eba401c992
Xuyang Zheng <tonyzheng@Xuyangs-MacBook-Pro.local>
2018-08-06 10:07:11 +1000

    fix

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname+'/dist/AdminWebpageV2'));
 
 
