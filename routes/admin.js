dc904de56bab218f6ed9dd7f6627cb758217c778
Xuyang Zheng <tonyzheng@Xuyangs-MacBook-Pro.local>
2018-08-10 13:58:29 +1000

    fix

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,11 @@ const path = require('path');
 
 
 router.get('/:id',(req,res)=>{
-    console.log(req.params.id);
     res.sendFile(path.join('/home/bglnewsdev00/NewsAPI/APITest/dist/AdminWebpageV2/'+req.params.id))
 });
 
 
 router.get('/',(req,res)=>{
-    console.log('get angular');
     res.sendFile(path.join('/home/bglnewsdev00/NewsAPI/APITest/dist/AdminWebpageV2/index.html'))
 });
 
