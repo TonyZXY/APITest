883c94f5d8a17fcdff69d2a4062cf24d48426770
Xuyang Zheng <tonyzheng@Xuyangs-MacBook-Pro.local>
2018-08-10 12:19:30 +1000

    add log

--- routes/admin.js
+++ routes/admin.js
@@ -12,6 +12,7 @@ router.get('/:id',(req,res)=>{
 
 
 router.get('/',(req,res)=>{
+    console.log('get angular');
     res.sendFile(path.join('/home/bglnewsdev00/NewsAPI/APITest/admin/admin/index.html'))
 });
 
