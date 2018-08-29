f8798c09fca2ee127c6b3e7f5b787fd9d4743d4d
TonyZheng <xyzheng109@gmail.com>
2018-08-29 11:41:11 +1000

    ..

---
+++ functions/insertUpdateData.js
@@ -0,0 +1,25 @@
+const Update = require('../module/UpdateInfo');
+const mongoose = require('mongoose');
+
+mongoose.connect(config.database, config.options);
+
+
+function add() {
+    let update = {
+        name: 'update',
+        version: '1.1(1)',
+        info: 'minor bug fix',
+        critical: false
+    };
+
+    Update.createUpdate(update,(err,msg)=>{
+        if (err){
+            console.log(err);
+        } else {
+            console.log(msg);
+        }
+    })
+}
+
+
+add();
