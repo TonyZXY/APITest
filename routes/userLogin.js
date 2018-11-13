3d16320d7db7657621bd5ec34371e66c960b5862
Tony Zheng <xyzheng109@gmail.com>
2018-11-13 14:49:39 +1100

    add debug

--- routes/userLogin.js
+++ routes/userLogin.js
@@ -206,7 +206,8 @@ function verifyToken(req, res, next) {
         let address = req.connection.remoteAddress;
         if (token === null || token === undefined ||
             email === null || email === undefined) {
-            logger.userRegistrationLoginLog(address, "Invalid param number"+ req.headers.toString());
+            logger.userRegistrationLoginLog(address, "Invalid param number");
+            console.log(req.headers);
             return res.send({
                 success: false,
                 message: "Token Error",
